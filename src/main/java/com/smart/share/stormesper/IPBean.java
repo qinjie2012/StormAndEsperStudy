0f2b69ddacf5635b2ea2f144cb3efdeaf0eedabc
qinjie2012 <qinjie2012@163.com>
2016-08-18 21:53:39 +0800

    add bean

---
+++ src/main/java/com/smart/share/stormesper/IPBean.java
@@ -0,0 +1,21 @@
+package com.smart.share.stormesper;
+/**
+ * NetWork IP Status bean
+ * @author liujie
+ *
+ * 2016年8月18日
+ *
+ */
+public class IPBean {
+
+	private long ID;
+	private String ipaddress;
+	private String productID;
+	private String clientID;
+	private String viewID;
+	private String appType;
+	private String eventType;
+	private String deviceType;
+	
+	
+}
